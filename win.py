7d5e1a2cc070aaf9404f869196104c0ed5e0c0a8
Ahmadfaisalabad805 <102409672+Ahmadfaisalabad805@users.noreply.github.com>
2022-05-21 13:10:01 +0500

    Create win.py

---
+++ win.py
@@ -0,0 +1,181 @@
+#its ahmad
+
+import os
+
+import sys
+
+import time
+
+import requests
+
+import random
+
+import platform
+
+import base64
+
+import subprocess
+
+from concurrent.futures import ThreadPoolExecutor
+
+def runtxt(z):
+
+    for e in z + "\n":
+
+        sys.stdout.write(e)
+
+        sys.stdout.flush()
+
+        time.sleep(0.03)
+
+def notice():
+
+	runtxt("\n        Admin :- Ahmad")	print("")
+
+	runtxt("\033[1;32m        Programer :- Faisalabadi")
+
+	print("")
+
+	runtxt("\033[1;33m        Team :- Ahmad + Mohsin + Imtaiz")
+
+	print("")
+
+	runtxt("\033[1;33m            🌹🌹🌹🌹🌹🌹🌹🌹🌹🌹")
+
+	print("")
+
+	runtxt("\033[1;36m          Pro Hackers Or Programers")
+
+	input()
+
+logo="""
+
+\033[1;31m █████  ██   ██ ███    ███  █████  ██████  
+
+██   ██ ██   ██ ████  ████ ██   ██ ██   ██ 
+
+\033[1;32m███████ ███████ ██ ████ ██ ███████ ██   ██ 
+
+██   ██ ██   ██ ██  ██  ██ ██   ██ ██   ██ 
+
+\033[1;36m██   ██ ██   ██ ██      ██ ██   ██ ██████  
+
+"""
+
+os.system('clear')
+
+print(logo)
+
+print("[•]•••••••••••••••••••••••••••••••••••••[•]")
+
+print ("\033[1;32m           Welcome 😁 To Lottery")
+
+print("")
+
+print("        Recommend Things To Enter Lotter👇")
+
+print("")
+
+print("\033[1;35m 1 :- subscribe like and comment")
+
+print("\033[1;36m 2 :- join group ")
+
+print("\033[1;37m 3 :- Send Name or Screen shot Subscribe ka")
+
+print("")
+
+print("\033[1;33m   [1] Subscribe Our Channel")
+
+Ahmad = input()
+
+if Ahmad in ["", " "]:
+
+    print (" [!] Please Select Correct Option")
+
+    exit()
+
+elif Ahmad in ["1", "01"]:
+
+    os.system('xdg-open https://youtu.be/muygYzQEoSg')
+
+    time.sleep(1.0)
+
+    os.system('clear')
+
+    print(logo)
+
+print("[•]•••••••••••••••••••••••••••••••••••••[•]")
+
+print("")
+
+print("        Recommend Things To Enter Lotter👇")
+
+print("")
+
+print("\033[1;35m 1 :- subscribe like and comment")
+
+print("\033[1;36m 2 :- join group ")
+
+print("\033[1;37m 3 :- Send Name or Screen shot Subscribe ka")
+
+print("")
+
+print("\033[1;36m   [2] Join Group")
+
+Ahmad = input()
+
+if Ahmad in [""]:
+
+    print (" [!] Please Select Correct Option")
+
+    exit()
+
+elif Ahmad in ["2", "02"]:
+
+	os.system('xdg-open https://chat.whatsapp.com/LLPexspdyAjFmXqJEUj1WL')
+
+	time.sleep(1.0)
+
+	os.system('clear')
+
+	print(logo)
+
+print("[•]•••••••••••••••••••••••••••••••••••••[•]")
+
+print("")
+
+print("        Recommend Things To Enter Lotter👇")
+
+print("")
+
+print("\033[1;35m 1 :- subscribe like and comment")
+
+print("\033[1;36m 2 :- join group ")
+
+print("\033[1;37m 3 :- Send Name or Screen shot Subscribe ka")
+
+print("")
+
+print("\033[1;32m   [?] Enter Your Name And Send Me")
+
+ah = input('\n\033[1;31m    Type Your Name :-\033[1;33m ')
+
+tks = 'Dear%20Sir,%20Please%20Add%20My%20Name%20In%20Lottery%20%20%20%20%20My%20%20Name%20%20:%20'+ah
+
+os.system('am start https://wa.me/+923109959710?text=' + tks)
+
+time.sleep(1.0)
+
+print('\n\033[1;32m   Congratulations Your Name Enter To Lottery ')
+
+print("")
+
+input("            \033[1;33m     Press Enter")
+
+os.system('clear')
+
+print(logo)
+
+print('        Wating For Anousment')
+
+notice()
